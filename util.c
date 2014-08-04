0764e1df23b0e371f5fe14ec8d1b1a568ec9d69d
Pim van Riezen <pi@madscience.nl>
2014-08-04 12:36:17 +0200

    ...

--- util.c
+++ util.c
@@ -3,3 +3,11 @@
 int uuidcomp (uuid_t first, uuid_t second) {
 	return (first.msb == second.msb && first.lsb == second.lsb);
 }
+
+uuid_t mkuuid (const char *str) {
+	uuid_t res = {0,0};
+	const char *c = str;
+	int respos = 0;
+	if (! c) return res;
+	
+}
