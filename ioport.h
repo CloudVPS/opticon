94fedba91bdb379babc984e6f3770f78925a9e08
Pim van Riezen <pi@madscience.nl>
2014-09-06 10:49:13 +0200

    whitespace

--- ioport.h
+++ ioport.h
@@ -13,29 +13,29 @@ typedef int (*writefunc)(struct ioport_s *, const char *, size_t);
 typedef void (*closefunc)(struct ioport_s *);
 
 typedef struct ioport_s {
-    writefunc	 write;
-    closefunc	 close;
-    void 		*storage;
-    uint8_t		 bitpos;
-    uint8_t		 bitbuffer;
+    writefunc    write;
+    closefunc    close;
+    void        *storage;
+    uint8_t      bitpos;
+    uint8_t      bitbuffer;
 } ioport;
 
 typedef struct bufferstorage_s {
-    char 		*buf;
-    size_t		 bufsz;
+    char        *buf;
+    size_t       bufsz;
     unsigned int pos;
 } bufferstorage;
 
 /* ============================= FUNCTIONS ============================= */
 
-ioport	*ioport_create_filewriter (FILE *);
-ioport	*ioport_create_buffer (char *buf, size_t sz);
+ioport  *ioport_create_filewriter (FILE *);
+ioport  *ioport_create_buffer (char *buf, size_t sz);
 void     ioport_close (ioport *io);
 int      ioport_write (ioport *io, const char *data, size_t sz);
-int		 ioport_write_uuid (ioport *io, uuid u);
-int		 ioport_write_byte (ioport *io, uint8_t b);
-int		 ioport_write_bits (ioport *io, uint8_t d, uint8_t numbits);
-int		 ioport_flush_bits (ioport *io);
+int      ioport_write_uuid (ioport *io, uuid u);
+int      ioport_write_byte (ioport *io, uint8_t b);
+int      ioport_write_bits (ioport *io, uint8_t d, uint8_t numbits);
+int      ioport_flush_bits (ioport *io);
 
 #endif
 
