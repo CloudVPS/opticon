c1a1bbf0bd1f300b7809502ad0f77e519d1dd6f0
Pim van Riezen <pi@madscience.nl>
2014-09-06 10:52:01 +0200

    tabtabtab

--- ioport.c
+++ ioport.c
@@ -44,17 +44,17 @@ ioport *ioport_create_buffer (char *buf, size_t sz) {
 }
 
 int ioport_write (ioport *io, const char *data, size_t sz) {
-	if (io->bitpos) ioport_flush_bits (io);
+    if (io->bitpos) ioport_flush_bits (io);
     return io->write (io, data, sz);
 }
 
 int ioport_write_byte (ioport *io, uint8_t b) {
-	if (io->bitpos) ioport_flush_bits (io);
+    if (io->bitpos) ioport_flush_bits (io);
     return io->write (io, (const char *) &b, 1);
 }
 
 int ioport_write_uuid (ioport *io, uuid u) {
-	return 0;
+    return 0;
 }
 
 void ioport_close (ioport *io) {
@@ -64,25 +64,25 @@ void ioport_close (ioport *io) {
 uint8_t BITMASKS[9] = {0x00, 0x01, 0x03, 0x07, 0x0f, 0x1f, 0x3f, 0x7f, 0xff};
 
 int ioport_write_bits (ioport *io, uint8_t d, uint8_t bits) {
-	if (bits>8) return 0;
-	uint8_t bits1 = (io->bitpos + bits < 8) ? bits : 8-io->bitpos;
-	uint8_t bits2 = bits - bits1;
-	uint8_t shift = (8 - io->bitpos) - bits1;
-	uint8_t data1 = (d & BITMASKS[bits1]) << shift;
-	uint8_t data2 = (d >> bits1) & BITMASKS[bits2];
-	io->bitbuffer |= data1;
-	io->bitpos += bits1;
-	if (io->bitpos > 7) {
-		if (! io->write (io, (const char *)&(io->bitbuffer), 1)) return 0;
-		io->bitpos = bits2;
-		io->bitbuffer = data2;
-	}
-	return 1;
+    if (bits>8) return 0;
+    uint8_t bits1 = (io->bitpos + bits < 8) ? bits : 8-io->bitpos;
+    uint8_t bits2 = bits - bits1;
+    uint8_t shift = (8 - io->bitpos) - bits1;
+    uint8_t data1 = (d & BITMASKS[bits1]) << shift;
+    uint8_t data2 = (d >> bits1) & BITMASKS[bits2];
+    io->bitbuffer |= data1;
+    io->bitpos += bits1;
+    if (io->bitpos > 7) {
+        if (! io->write (io, (const char *)&(io->bitbuffer), 1)) return 0;
+        io->bitpos = bits2;
+        io->bitbuffer = data2;
+    }
+    return 1;
 }
 
 int ioport_flush_bits (ioport *io) {
-	if (! io->bitpos) return 1;
-	if (! io->write (io, (const char *)&(io->bitbuffer), 1)) return 0;
-	io->bitpos = io->bitbuffer = 0;
-	return 1;
+    if (! io->bitpos) return 1;
+    if (! io->write (io, (const char *)&(io->bitbuffer), 1)) return 0;
+    io->bitpos = io->bitbuffer = 0;
+    return 1;
 }
