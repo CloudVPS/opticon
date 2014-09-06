e2daa8e75c91bd3bdbf008fe66af8f2445abec1c
Pim van Riezen <pi@madscience.nl>
2014-09-06 13:52:33 +0200

    refactor the json stuff into codec.c

--- util.h
+++ util.h
@@ -10,8 +10,8 @@ int          uuidcmp (uuid first, uuid second);
 uuid         mkuuid (const char *str);
 void         uuid2str (uuid u, char *into);
 void         id2str (meterid_t id, char *into);
+void         nodeid2str (meterid_t id, char *into);
 uint64_t     idhaspath (meterid_t);
 meterid_t    makeid (const char *label, metertype_t type, int pos);
-void         dump_host_json (host *, ioport *);
 
 #endif
