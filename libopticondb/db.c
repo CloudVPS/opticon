3f8659527a5c912409d3ecdf3a94536d2e4d5981
Pim van Riezen <pi@madscience.nl>
2014-09-08 09:26:48 +0200

    Further organization of libopticondb

--- libopticondb/db.c
+++ libopticondb/db.c
@@ -1,15 +1,5 @@
 #include <db.h>
 
-db *db_open_local (const char *path) {
-    localdb *res = (localdb *) malloc (sizeof (localdb));
-    res->db.get_record = localdb_get_record;
-    res->db.get_value_range_int = localdb_get_value_range_int;
-    res->db.get_value_range_frac = localdb_get_value_range_frac;
-    res->db.save_record = localdb_save_record;
-    res->path = strdup (path);
-    return (db *) res;
-}
-
 int db_get_record (db *d, time_t when, host *into) {
     return d->get_record (d, when, into);
 }
