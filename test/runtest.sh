6767809398eac79d1d3ae311336939612ad0e78e
Pim van Riezen <pi@madscience.nl>
2014-09-12 17:19:16 +0200

    aesthetica

--- test/runtest.sh
+++ test/runtest.sh
@@ -12,7 +12,8 @@ CC="$*"
 for test in test_*.c; do
   echo "====================================================================="
   ofile=$(echo "$test" | sed -e 's/\.c$//')
-  $ECHON "* COMPILE ${ofile}...$NNL"
+         
+  $ECHON "[BUILD] ${ofile}...$NNL"
   make ${ofile}.o >out/${ofile}.build 2>&1
   if [ ! $? = 0 ]; then
     echo " FAIL"
@@ -29,21 +30,21 @@ for test in test_*.c; do
   fi
   rm -f out/${ofile}.build
   echo " OK"
-  echo "* RUNTEST ${ofile}:"  
+  echo "[CHECK] ${ofile}:"  
   bin/${ofile} > out/${ofile}.out
   if [ ! $? = 0 ]; then
-    echo "  FAIL(run)"
+    echo "[FAIL ] Process terminated with error status"
     exit 1
   fi
   if [ -e ${ofile}.stdout ]; then
     diff -q ${ofile}.stdout out/${ofile}.out >/dev/null
     if [ ! $? = 0 ]; then
-      echo "  FAIL(diff)"
+      echo "[FAIL ] Output differed from expected output:"
       diff -c ${ofile}.stdout out/${ofile}.out
       rm -f out/${ofile}.out
       exit 1
     fi
   fi
-  echo "* ENDTEST ${ofile}"
+  echo "[DONE ] ${ofile}"
   rm -f out/${ofile}.out
 done
