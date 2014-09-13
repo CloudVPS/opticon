4eca393c04f3d0f659b1b42bd111b4a67a949e8a
Pim van Riezen <pi@madscience.nl>
2014-09-13 18:34:56 +0200

    Add cancel and cleanup testing to test_thread.

--- test/test_thread.c
+++ test/test_thread.c
@@ -1,15 +1,26 @@
 #include <libsvc/thread.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <assert.h>
 
 int COUNTER=0;
+int CLEANED=0;
+
+void cleanup (thread *t) {
+    CLEANED++;
+}
 
 void run (thread *t) {
+    COUNTER++;
+    sleep (4);
     COUNTER++;
 }
 
 int main (int argc, const char *argv[]) {
-    thread *t = thread_create (run, NULL);
+    thread *t = thread_create (run, cleanup);
     while (! COUNTER) sleep(1);
+    thread_cancel (t);
+    assert (COUNTER == 1);
+    assert (CLEANED == 1);
     return 0;
 }
