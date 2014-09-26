8c644df49ecf405cce7a487c42342cda51c53dbf
Pim van Riezen <pi@cloudvps.com>
2014-09-26 15:11:04 +0200

    cut some dead wood

--- src/opticon-agent/probes.c
+++ src/opticon-agent/probes.c
@@ -3,12 +3,6 @@
 #include <libopticonf/var.h>
 #include "probes.h"
 
-var *runprobe_pcpu (probe *self) {
-    var *res = var_alloc();
-    var_set_double_forkey (res, "pcpu", ((double)(rand()%1000))/10.0);
-    return res;
-}
-
 var *runprobe_hostname (probe *self) {
     char out[256];
     gethostname (out, 255);
@@ -38,7 +32,7 @@ var *runprobe_uname (probe *self) {
 
 /** List of built-in probe functions */
 builtinfunc BUILTINS[] = {
-    {"probe_pcpu", runprobe_top},
+    {"probe_top", runprobe_top},
     {"probe_hostname", runprobe_hostname},
     {"probe_uname", runprobe_uname},
     {"probe_df", runprobe_df},
