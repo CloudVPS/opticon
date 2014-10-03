38cb9361c2d01b1069666c943a7870b9d51052b2
Pim van Riezen <pi@madscience.nl>
2014-10-03 09:48:48 +0200

    move hash_token() to libopticon

---
+++ include/libopticon/hash.h
@@ -0,0 +1,12 @@
+#ifndef _HASH_H
+#define _HASH_H 1
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <assert.h>
+#include <fcntl.h>
+
+uint32_t hash_token (const char *);
+
+#endif
--- src/opticon-api/tokencache.c
+++ src/opticon-api/tokencache.c
@@ -4,59 +4,10 @@
 #include <assert.h>
 #include <fcntl.h>
 #include <libopticon/util.h>
+#include <libopticon/hash.h>
 #include "tokencache.h"
 
-static uint32_t IHASH = 0;
 tokencache TOKENCACHE;
-
-/** Generic hash-function nicked from grace.
-  * \param str The string to hash
-  * \return A 32 bit hashed guaranteed not 0.
-  */
-uint32_t hash_token (const char *str) {
-    uint32_t hash = 0;
-    uint32_t i    = 0;
-    uint32_t s    = 0;
-    int sdf;
-    
-    /* Set up a random intial hash once */
-    if (! IHASH)
-    {
-        sdf = open ("/dev/urandom", O_RDONLY);
-        if (sdf<0) sdf = open ("/dev/random", O_RDONLY);
-        if (sdf>=0)
-        {
-            (void) read (sdf, &IHASH, sizeof(IHASH));
-            close (sdf);
-        }
-        if (! IHASH) IHASH = 5138; /* fair dice roll */
-    }
-    
-    hash = IHASH;
-    
-    const unsigned char* ustr = (const unsigned char*)str;
-
-    for (i = 0; ustr[i]; i++) {
-        hash = ((hash << 5) + hash) + (ustr[i] & 0xDF);
-    }
-
-    s = hash;
-    
-    switch (i) { /* note: fallthrough for each case */
-        default:
-        case 6: hash += (s % 5779) * (ustr[6] & 0xDF);
-        case 5: hash += (s % 4643) * (ustr[5] & 0xDF); hash ^= hash << 7;
-        case 4: hash += (s %  271) * (ustr[4] & 0xDF);
-        case 3: hash += (s % 1607) * (ustr[3] & 0xDF); hash ^= hash << 15;
-        case 2: hash += (s % 7649) * (ustr[2] & 0xDF);
-        case 1: hash += (s % 6101) * (ustr[1] & 0xDF); hash ^= hash << 25;
-        case 0: hash += (s % 1217) * (ustr[0] & 0xDF);
-    }
-    
-    /* zero is inconvenient */
-    if (hash == 0) hash = 154004;
-    return hash;
-}
 
 /** Reset a tcache_node to unset.
   * \param self The node to clear
