c5a316d363f4be9a4a45f40f001a86cb5bd2d5e0
Pim van Riezen <pi@madscience.nl>
2014-10-05 12:33:39 +0200

    dito for the temp file

--- test/test_daemon.c
+++ test/test_daemon.c
@@ -7,12 +7,14 @@
 #include <stdlib.h>
 #include <assert.h>
 
+char TMPF[128];
+
 int daemonfunc (int argc, const char *argv[]) {
     char buf[128];
     int i;
-    FILE *f = fopen ("./.tmp-file","r");
+    FILE *f = fopen (TMPF,"r");
     if (! f) {
-        f = fopen ("./.tmp-file","w");
+        f = fopen (TMPF,"w");
         fprintf (f, "%i", 1);
         fclose (f);
         return 0;
@@ -21,7 +23,7 @@ int daemonfunc (int argc, const char *argv[]) {
     fclose (f);
     i = atoi (buf);
     if (i == 1) {
-        f = fopen ("./.tmp-file","w");
+        f = fopen (TMPF,"w");
         fprintf (f, "2");
         fclose (f);
     }
@@ -30,19 +32,20 @@ int daemonfunc (int argc, const char *argv[]) {
 
 int main (int argc, const char *argv[]) {
     char buf[128];
-    char TMPF[128];
+    char PIDF[128];
     pid_t p;
     FILE *f;
     
     uuid u = uuidgen();
     uuid2str (u, buf);
-    sprintf (TMPF, "/tmp/%s.pid", buf);
+    sprintf (PIDF, "/tmp/%s.pid", buf);
+    sprintf (TMPF, "/tmp/%s.file", buf);
     
-    assert (daemonize (TMPF, argc, argv, daemonfunc, 0));
+    assert (daemonize (PIDF, argc, argv, daemonfunc, 0));
     log_info ("Giving the daemon some time");
     sleep (2);
     
-    assert (f = fopen (TMPF, "r"));
+    assert (f = fopen (PIDF, "r"));
     fgets (buf, 128, f);
     fclose (f);
     
@@ -51,19 +54,19 @@ int main (int argc, const char *argv[]) {
     assert (kill (p, SIGTERM) == 0);
     sleep (1);
     log_info ("Checking on leftovers");
-    f = fopen (TMPF,"r");
+    f = fopen (PIDF,"r");
     int cnt = 0;
     while (cnt < 5 && f) {
         fclose (f);
         sleep (1);
-        f = fopen (TMPF,"r");
+        f = fopen (PIDF,"r");
         cnt++;
     }
     assert (f == NULL);
-    assert (f = fopen ("./.tmp-file","r"));
+    assert (f = fopen (TMPF,"r"));
     fgets (buf, 128, f);
     fclose (f);
-    unlink ("./.tmp-file");
+    unlink (TMPF);
     assert (atoi (buf) == 2);
     return 0;
 }
