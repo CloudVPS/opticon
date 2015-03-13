a269db99d020efdf2d15e2d345fb6cc440e97c20
Pim van Riezen <pi@cloudvps.com>
2015-03-13 10:38:08 +0100

    Clean up notify.h/notify.c

--- include/libopticon/notify.h
+++ include/libopticon/notify.h
@@ -1,6 +1,12 @@
 #ifndef _NOTIFY_H
 #define _NOTIFY_H
 
+#include <stdbool.h>
+#include <stdint.h>
+#include <libopticon/uuid.h>
+
+/* =============================== TYPES =============================== */
+
 typedef struct notification_s {
     struct notification_s   *next;
     struct notification_s   *prev;
@@ -15,5 +21,16 @@ typedef struct notifylist_s {
     notification        *first;
     notification        *last;
 } notifylist;
+
+/* ============================= FUNCTIONS ============================= */
+
+notification *notification_create (void);
+void          notification_delete (void);
+void          notifylist_init (notifylist *);
+void          notifylist_remove (notifylist *, notification *);
+void          notifylist_link (notifylist *, notification *);
+notification *notifylist_find (notifylist *, uuid);
+bool          notifylist_check_actionable (notifylist *);
+notification *notifylist_find_overdue (notifylist *self, notification *n);
 
 #endif
--- src/libopticon/notify.c
+++ src/libopticon/notify.c
@@ -1,3 +1,8 @@
+#include <libopticon/notify.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/** Allocate and initialize a notification object */
 notification *notification_create (void) {
     notification *res = (notification *) malloc (sizeof (notification));
     res->next = res->prev = NULL;
@@ -5,14 +10,25 @@ notification *notification_create (void) {
     res->lastchange = time (NULL);
 }
 
+/** Free up memory for a notification object. Should be removed from its
+  * list using notifylist_remove() first.
+  * \param self The notification object.
+  */
 void notification_delete (notification *self) {
     free (self);
 }
 
+/** Initialize a notifylist object.
+  * \param self The notifylist object to initialize.
+  */
 void notifylist_init (notifylist *self) {
     self->first = self->last = NULL;
 }
 
+/** Removes a notification object from a notifylist.
+  * \param self The list to remove from.
+  * \param n The notification object to unlink.
+  */
 void notifylist_remove (notifylist *self, notification *n) {
     if (n->prev) {
         if (n->next) {
@@ -37,6 +53,10 @@ void notifylist_remove (notifylist *self, notification *n) {
     notification_delete(n);
 }
 
+/** Add a notification object to a notifylist.
+  * \param self The notifylist to add to.
+  * \param n The notification object to link.
+  */
 void notifylist_link (notifylist *self, notification *n) {
     n->next = NULL;
     if (self->last) {
@@ -49,6 +69,11 @@ void notifylist_link (notifylist *self, notification *n) {
     }
 }
 
+/** Find a specific host in a tenant's notifylist.
+  * \param self The notifylist to search.
+  * \param hostid The host uuid to search for.
+  * \return Pointer to the notification object, or NULL if not found.
+  */
 notification *notifylist_find (notifylist *self, uuid hostid) {
     notification *n = self->first;
     while (n) {
@@ -58,6 +83,12 @@ notification *notifylist_find (notifylist *self, uuid hostid) {
     return n;
 }
 
+/** Find out if there are actionable notifications in a notifylist.
+  * The definition of 'actionable' is: At least one notification has
+  * been queued for at least 290 seconds.
+  * \param self The notifylist to inspect.
+  * \return true if there's action afoot.
+  */
 bool notifylist_check_actionable (notifylist *self) {
     notification *n = self->first;
     time_t tnow = time (NULL);
@@ -68,6 +99,17 @@ bool notifylist_check_actionable (notifylist *self) {
     return false;
 }
 
+/** Get the first, or next, notification in a notifylist that should be
+  * included in an outgoing message. Any notification that has been
+  * around for more than 2 minutes is considered in this stage. This
+  * function is not supposed to be called before getting a positive result
+  * out of notifylist_check_actionable(), so the end result is that
+  * a message will be sent for all notifications due for at least 2 minutes,
+  * as long as there is one that is due for at least 5 minutes.
+  * \param self The notifylist to search.
+  * \pram n The last notification object returned, or NULL to start a new
+  *         round.
+  */
 notification *notifylist_find_overdue (notifylist *self, notification *n) {
     if (! n) n = self->first;
     if (! n) return NULL;
